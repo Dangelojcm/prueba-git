d7828de88ab07feacc54b1b523c8d5a9f6800516
Dangelojcm <dangelocatacora30@gmail.com>
2026-08-11 22:31:46 -0500

    Agrego primeros archivos de prueba: html y python

---
+++ scripts/algoritmo1.py
@@ -0,0 +1,12 @@
+def es_primo(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+print("Números primos del 1 al 30:")
+for num in range(1, 31):
+    if es_primo(num):
+        print(num)
